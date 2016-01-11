f6324dd0bf3d272ac7cd8dd935e0d39bd62854d4
Christian Nielsen <blackapple.chr@gmail.com>
2016-01-11 01:54:59 +0100

    gimme moar

--- Graph.py
+++ Graph.py
@@ -1,11 +1,15 @@
 import numpy as np
 import pprint
 
+pp = pprint.PrettyPrinter(depth=6)
+
 class Graph(object):
 
     def __init__(self):
         self._G = {}
         self._V = []
+
+    #def __iter__(self): return self._G.itervalues()
 
     def __str__(self):
         res = ""
@@ -14,7 +18,10 @@ class Graph(object):
             adj = self._G[v]
             for w in adj:
                 new_res = "(" + str(v) + "->" + str(w) + "=" + str(self.get_edge_weight(v,w)) + ")"
-                res = res + ", " + new_res
+                if len(res) == 0:
+                    res = new_res
+                else:
+                    res = res + ", " + new_res
         return res
 
     def add_vertex(self, vertex):
@@ -25,6 +32,9 @@ class Graph(object):
         self._G[source][target] = weight
         self._G[target][source] = weight
 
+    def get_graph(self):
+        return self._G
+        
     def get_vertices(self):
         return self._V
 
@@ -32,13 +42,31 @@ class Graph(object):
         w = self._G[source][target]
         return w
 
+    def test(self):
+        return self._G
+
 if __name__ == '__main__':
 
     G = Graph()
 
-    G.add_vertex(1)
-    G.add_vertex(2)
+    G.add_vertex('1')
+    G.add_vertex('2')
+    G.add_vertex('3')
+    G.add_vertex('4')
 
-    G.add_edge(1,2,100)
+    G.add_edge('1','2',10)
+    G.add_edge('1','3',15)
+    G.add_edge('1','4',45)
+    G.add_edge('2','3',30)
+    G.add_edge('2','4',50)
+    G.add_edge('3','4',20)
 
-    print(G)
+
+    print G.test()['1']
+    A = {'s': {'u':10, 'x':5},
+    	'u': {'v':1, 'x':2},
+    	'v': {'y':4},
+    	'x':{'u':3,'v':9,'y':2},
+    	'y':{'s':7,'v':6}}
+    #pp.pprint(A)
+    #print(G)
---
+++ gen.py
@@ -0,0 +1,25 @@
+from Graph import Graph
+class GraphGen(object):
+    def __init__(self, vertices, density, is_weighted):
+        if density <= 0.0:
+            Exception("Density too low")
+        elif vertices < 2:
+            Exception("Too few vertices")
+        elif density > 1.0:
+            density = 1.0
+
+        self.vertices = vertices
+        self.density = density
+        self.is_weighted = is_weighted
+        self.graph = Graph()
+
+    def gen_graph(self):
+        for v in xrange(0, self.vertices):
+            v = "v" + v.__str__()
+            self.graph.Graph.add_vertex(v)
+
+
+
+
+if __name__ == '__main__':
+    print GraphGen(4, 1.0, True).gen_graph()
